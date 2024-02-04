328f8f6695913671f1d31bf3b8736a51ecd2f49f
Dhruv <dhruvanandsingh2004@gmail.com>
2024-02-04 22:49:33 +0530

    made structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element= {<Home />}/>
+    <Route path='/search' element= {<Search />}/>
+    <Route path='/watch/:id' element= {<Watch />}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Watch.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Watch() {
+  return (
+    <div>
+      Wattch
+    </div>
+  )
+}
